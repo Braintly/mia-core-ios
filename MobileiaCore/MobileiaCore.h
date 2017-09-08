4c38b74a88bcbacbc1f2e009ae75e84b29f533dd
Matias Camiletti <matias.camiletti@gmail.com>
2017-09-08 17:09:12 -0300

    Initial Commit

---
+++ MobileiaCore/MobileiaCore.h
@@ -0,0 +1,19 @@
+//
+//  MobileiaCore.h
+//  MobileiaCore
+//
+//  Created by Matias Camiletti on 8/9/17.
+//  Copyright © 2017 Matias Camiletti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobileiaCore.
+FOUNDATION_EXPORT double MobileiaCoreVersionNumber;
+
+//! Project version string for MobileiaCore.
+FOUNDATION_EXPORT const unsigned char MobileiaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobileiaCore/PublicHeader.h>
+
+
