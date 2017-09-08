15b85a80c6d9e91f143c19b38cb3448185d6bd5d
Matias Camiletti <matias.camiletti@gmail.com>
2017-09-08 17:14:20 -0300

    Commit inicial

---
+++ MobileiaCore/Mobileia.swift
@@ -0,0 +1,20 @@
+//
+//  Mobileia.swift
+//  MobileiaCore
+//
+//  Created by Matias Camiletti on 8/9/17.
+//  Copyright © 2017 Matias Camiletti. All rights reserved.
+//
+
+import Foundation
+
+class Mobileia {
+    static var instance : Mobileia = Mobileia()
+    
+    var appId : Int = 0;
+    var deviceToken : String = "";
+    
+    static func getInstance() -> Mobileia {
+        return instance;
+    }
+}
