516d64abc0fb043b200aa16a71d07485bf73e62a
Matias Camiletti <matias.camiletti@gmail.com>
2018-05-19 19:25:01 -0300

    Agregado funcionalidad para detectar cuando se abre el teclado.

--- MobileiaCore/Controller/MIAViewController.swift
+++ MobileiaCore/Controller/MIAViewController.swift
@@ -9,8 +9,11 @@
 import UIKit
 
 open class MIAViewController<T : MIAView>: UIViewController {
-
+    
+    // Almacena la vista personalizada
     open var miaView : T!;
+    // Almacena el constraint de la parte inferior de la vista
+    open var constraintBottom : NSLayoutConstraint!;
     
     open override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,26 +31,59 @@ open class MIAViewController<T : MIAView>: UIViewController {
         // Agregamos la vista al controlador
         self.view.addSubview(miaView)
         // AutoLayout
-        miaView.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets.zero)
+        miaView.autoPinEdge(toSuperviewEdge: .top, withInset: 0);
+        constraintBottom = miaView.autoPinEdge(toSuperviewEdge: .bottom, withInset: 0);
+        miaView.autoPinEdge(toSuperviewEdge: .left, withInset: 0);
+        miaView.autoPinEdge(toSuperviewEdge: .right, withInset: 0);
+        // Asignamos para que detecte cuando se abre el teclado
+        setupViewResizerOnKeyboardShown();
     }
 
     open override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    deinit {
+        // Eliminar notificacion del teclado
+        removeForKeyboardNotifications();
+    }
+    
     // MARK: - Class Methods
     // Funcion que se encarga de generar el controlador por defecto
     open class func factoryController() -> MIAViewController {
         return self.init();
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - Keyboard Functions
+    func setupViewResizerOnKeyboardShown() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillShowForResizing),
+                                               name: Notification.Name.UIKeyboardWillShow,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillHideForResizing),
+                                               name: Notification.Name.UIKeyboardWillHide,
+                                               object: nil)
     }
-    */
+    
+    @objc func keyboardWillShowForResizing(notification: Notification) {
+        let info : NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info.object(forKey: UIKeyboardFrameEndUserInfoKey) as AnyObject).cgRectValue.size
+        
+        if keyboardSize.height != 0 {
+            self.constraintBottom.constant = -keyboardSize.height;
+            self.miaView.layoutIfNeeded();
+        }
+    }
+    
+    @objc func keyboardWillHideForResizing(notification: Notification) {
+        self.constraintBottom.constant = 0;
+        self.miaView.layoutIfNeeded();
+    }
+    
+    func removeForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
 }
